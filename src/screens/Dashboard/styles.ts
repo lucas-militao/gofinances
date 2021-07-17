ee4de2172c7230e2db4b370f0b9475e2db2499ef
lumilitas <lucasmengineer@edu.unifor.br>
2021-07-17 11:04:04 -0300

    Acessando propriedades no Styled Components

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -11,7 +12,7 @@ export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(42)}px;
 
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
   flex-direction: row;
 
@@ -22,6 +23,7 @@ export const UserWrapper = styled.View`
   width: 100%;
 
   padding: 0px 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
 
   flex-direction: row;
   justify-content: space-between;
@@ -57,4 +59,12 @@ export const UserName = styled.Text`
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secondary};
   font-size: ${RFValue(24)};
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 }
+})`
+  
 `;
