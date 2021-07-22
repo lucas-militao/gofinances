5e2b4caee7664eb2c5eb1a1c3395be797f7b3a9c
lumilitas <lucasmengineer@edu.unifor.br>
2021-07-22 08:40:00 -0300

    Finalizada a listagem de transações

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import { HighlightCard } from '../../components/HighlightCard';
-import TransactionCard from '../../components/TransactionCard';
+import TransactionCard, { TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
   Container,
@@ -20,8 +19,13 @@ import {
   TransactionsList
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
-  const data = [{
+  const data: DataListProps[] = [{
+    id: '1',
     type: "positive",
     title:"Desenvolvimento de site",
     amount:"R$ 12.000,00",
@@ -32,6 +36,7 @@ export function Dashboard() {
     date:"13/04/2020"
   },
   {
+    id: '2',
     type: "negative",
     title:"Hamburgueria Pizzy",
     amount:"R$ 59,00",
@@ -42,6 +47,7 @@ export function Dashboard() {
     date:"10/04/2020"
   },
   {
+    id: '3',
     type: "negative",
     title:"Aluguel do aparmento",
     amount:"R$ 1.200,00",
@@ -100,11 +106,8 @@ export function Dashboard() {
 
         <TransactionsList 
           data={data}
-          renderItem={({ item }) => <TransactionCard data={item}/>}
-          showsVerticalScrollIndicator={false}  
-          contentContainerStyle={{
-            paddingBottom: getBottomSpace()
-          }}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item}/>}      
         />
       </Transactions>
     </Container>
