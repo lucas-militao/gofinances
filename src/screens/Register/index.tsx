39c9c297be81106ad5453d5326f2c92bcf98e974
lumilitas <lucasmengineer@edu.unifor.br>
2021-07-22 15:48:30 -0300

    Implementando o componente de select

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { Button } from '../../components/Forms/Button';
+import { CategorySelect } from '../../components/Forms/CategorySelect';
 import { Input } from '../../components/Forms/Input';
 import { TransactionTypeButton } from '../../components/TransactionTypeButton';
 
@@ -46,6 +47,8 @@ export function Register() {
               isActive={transactionType === 'down'}
             />
           </TransactionsTypes>
+
+          <CategorySelect title="Categoria"/>
         </Fields>
 
         <Button title="Enviar"/>
