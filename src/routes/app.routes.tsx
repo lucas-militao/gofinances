8b6ee37afdc67a990be8d82ad888896e7c1706ba
lumilitas <lucasmengineer@edu.unifor.br>
2021-07-25 10:12:09 -0300

    Implementando a interface do gráfico

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { Register } from "../screens/Register";
 import { useTheme } from "styled-components";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Platform } from "react-native";
+import { Resume } from "../screens/Resume";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -50,7 +51,7 @@ export function AppRoutes() {
       />
       <Screen 
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: (({ size, color }) => (
             <MaterialIcons 
