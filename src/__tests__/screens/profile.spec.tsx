9454bcc749dea8f9b64d92b11041b653333721ae
lumilitas <lucasmengineer@edu.unifor.br>
2021-09-23 08:00:16 -0300

    Implementado o primeiro exemplo de teste

---
+++ src/__tests__/screens/profile.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import { Profile } from '../../screens/Profile';
+
+test('check if show correctly user input name button placeholder', () => {
+  const { getByPlaceholderText } = render(<Profile />);
+
+  const inputName = getByPlaceholderText('Nome');
+  
+  expect(inputName.props.placeholder).toBeTruthy();
+
+});
+
