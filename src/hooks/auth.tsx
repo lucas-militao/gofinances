c966344ffff9ac8a8f0825eff30e581dbb6e4cec
lumilitas <lucasmengineer@edu.unifor.br>
2021-09-29 13:26:43 -0300

    Implementado o teste com login social

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -55,20 +55,22 @@ function AuthProvider({ children }: AuthProviderProps) {
       const SCOPE = encodeURI('profile email');
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
-
+      
       const { type, params } = await AuthSession
         .startAsync({ authUrl }) as AuthorizationResponse;
-      
+        
       if (type === 'success') {
         const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        
         const userInfo = await response.json();
-
+        
         const userLogged: User = {
           id: userInfo.id,
           email: userInfo.email,
           name: userInfo.given_name,
           photo: userInfo.picture
         }
+        
 
         setUser(userLogged);
 
