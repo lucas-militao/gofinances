0c444e37608ea845070b6b0cf0c9f9c5925beb16
lumilitas <lucasmengineer@edu.unifor.br>
2021-09-29 13:19:43 -0300

    Teste de login social implementado

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,8 +1,43 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from './auth';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+jest.mock('expo-auth-session', () => {
+  return {
+    startAsync: () => {
+      return {
+        type: 'success',
+        params: {
+          access_token: 'token'
+        },
+      }
+    },
+  }
+});
+
+
 describe('Auth Hook', () => {
+
   it('should be able to sign in with Google account existing', async () => {
+    global.fetch = jest.fn(() => new Promise<any>(resolve => {  
+      resolve({ json: () => {
+        return {
+          id: 'userInfo.id',
+          email: 'userInfo.email',
+          name: 'userInfo.given_name',
+          photo: 'userInfo.picture'
+        } as User
+      }
+    })
+      }) 
+    );
+
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
     });
